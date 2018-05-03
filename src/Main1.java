ff72b452ed467edec1486e22e66c4fa48b513757
JoaquinDiazRamirez <joaco_diaz_ramirez@hotmail.com>
2018-05-03 14:06:47 +0200

    Commit de prueba

---
+++ src/Main1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main1 {
+    private JPanel Hola;
+    private JTextField Nombre;
+    private JTextField Apellidos;
+    private JCheckBox androidCheckBox;
+    private JCheckBox appleCheckBox;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
